ea73e48245890ede7f23608fa94afccde0b50620
Tibroux <Tiby@batbookrodetiby.home>
2015-04-27 15:02:53 +0200

    RAAAHHHHHH !

--- inscription.php
+++ inscription.php
@@ -1,12 +1,50 @@
+<?php
+
+function is_valid_email($email) {
+	return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+//form processing
+if($_POST) {
+	// nettoyage
+	$username = strip_tags(trim($_POST['username']));
+	$password = strip_tags(trim($_POST['password']));
+	$school = strip_tags(trim($_POST['school']));
+	$email = strip_tags(trim($_POST['email']));
+	$confirm_email = strip_tags(trim($_POST['confirm_email']));
+	
+	if($username == '') {
+		echo "*Nom d'utilisateur";
+	}
+	if($password == '') {
+		echo "*Mot de passe";
+	}
+	if(is_valid_email($email) == false) {
+		echo '*Email';
+	}
+	if(is_valid_email($confirm_email) == false) {
+		echo '*Confirmation de votre email';
+	}
+	else {
+	//ici l'envoi d'email
+	// on termine
+	echo 'Merci de votre inscription.';
+	INSERT INTO users(username,password,school,email,confirm_email) VALUES('$username','$password','$email','$confirm_email');
+	}
+}
+
+
+?>
+
 <!doctype html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Inscription</title>
-    <!--<link rel="stylesheet" href="css/styles.css" type="text/css"/>-->
+    <link rel="stylesheet" href="css/styles.css" type="text/css"/>
 </head>
 <body>
-	<header>
+	<header class="phone">
 		<ul>
 			<li class="back"><a href="#">Accueil</a></li>
 			<li><h1>Inscription</h1></li>
@@ -14,65 +52,67 @@
 		</ul>
 	</header>
 	<div class="content">
-		<form class="synchro" action="post">
+		<div class="phone">
+			<form class="synchro" method="post">
 			<fieldset>
 				<legend>Synchronisation</legend>
 				<ol>
-					<li>
-					<label for="ical">iCal</label>
+					<li id="first_decal_synchro" class="decal">
 					<input id="ical" name="ical" type="checkbox"/>
+					<label for="ical">iCal</label>
 					</li>
-					<li>
-					<label for="googlecal">Google Calendar</label>
+					<li class="decal">
 					<input id="googlecal" name="googlecal" type="checkbox"/>
+					<label for="googlecal">Google Calendar</label>
 					</li>
-					<li>
+					<li class="decal">
+					<input id="extranet" name="extranet" type="checkbox" checked/>
 					<label for="extranet">Extranet Étudiant</label>
-					<input id="extranet" name="extranet" type="checkbox"/>
 					<ol>
 						<li>
-						<label for="link_extranet">Lien extranet</label>
-						<input id="link_extranet" name="link_extranet" type="text" placeholder="Lien extranet..."/></li>
+						<label class="disappear" for="link_extranet">Lien extranet</label>
+						<input id="link_extranet" class="area" name="link_extranet" type="text" placeholder="Lien extranet..."/></li>
 					</ol>
 					</li>
 				</ol>
 			</fieldset>
 		</form>
-		<form class="compte" action="post">
+		<form class="compte" method="post">
 			<fieldset>
 				<legend>Compte</legend>
 				<ol>
-					<li>
-					<label for="username">Nom d'utilisateur</label>
-					<input id="username" name="username" type="text" placeholder="Nom d'utilisateur..."/>
+					<li id="first_decal_compte" class="decal">
+					<label class="disappear" for="username">Nom d'utilisateur</label>
+					<input id="username" class="area" name="username" type="text" placeholder="Nom d'utilisateur..."/>
 					</li>
-					<li>
-					<label for="password">Mot de passe</label>
-					<input id="password" name="password" type="text" placeholder="Mot de passe..."/>
+					<li class="decal">
+					<label class="disappear" for="password">Mot de passe</label>
+					<input id="password" class="area" name="password" type="text" placeholder="Mot de passe..."/>
 					</li>
-					<li>
-					<label for="school">École</label>
-					<input id="school" name="school" type="text" placeholder="École..."/>
+					<li class="decal">
+					<label class="disappear" for="school">École</label>
+					<input id="school" class="area" name="school" type="text" placeholder="École..."/>
 					</li>
-					<li>
-					<label for="email">E-mail</label>
-					<input id="email" name="email" type="text" placeholder="E-mail..."/>
+					<li class="decal">
+					<label class="disappear" for="email">E-mail</label>
+					<input id="email" class="area" name="email" type="text" placeholder="E-mail..."/>
 					</li>
-					<li>
-					<label for="confirm_email">Confirmation e-mail</label>
-					<input id="confirm_email" name="confirm_email" type="text" placeholder="Confirmation e-mail..."/>
+					<li class="decal">
+					<label class="disappear" for="confirm_email">Confirmation e-mail</label>
+					<input id="confirm_email" class="area" name="confirm_email" type="text" placeholder="Confirmation e-mail..."/>
 					</li>
-					<li>
-					<label for="localisation">Localisation</label>
+					<li class="simule">
 					<input id="localisation" name="localisation" type="checkbox"/>
+					<label for="localisation">Localisation</label>
 					</li>
-					<li>
-					<label for="notifications">Notifications</label>
+					<li id="last_compte" class="simule">
 					<input id="notifications" name="notifications" type="checkbox"/>
+					<label for="notifications">Notifications</label>
 					</li>
 				</ol>
 			</fieldset>
 		</form>
+		</div>
 	</div>
 </body>
 </html>
