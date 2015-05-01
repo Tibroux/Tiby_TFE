814e3aa071224cbfd5119c79ff27ecae3ca880bc
Tibroux <Tiby@batbookrodetiby.home>
2015-05-01 12:56:10 +0200

    corr_backup

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+require 'config.inc.php';
+
+
+if($_SESSION['logged_in'] != 'ok'){
+
+	if(isset($_POST['connexion'])){
+		
+		//sanatisation
+		$username = trim(strip_tags($_POST['username']));
+		$password = trim(strip_tags($_POST['password']));
+		$errors = array();
+		
+		//validation (si username est vide, on affiche error. Si password est vide, de même.)
+		
+		if( $username == '') {
+            $errors['username'] = 'Nom d\'utilisateur ?';
+        }
+    	if ( $password == '') {
+			$errors['password'] = 'Mot de passe ?';
+    	}
+    
+		// Si pas d'erreurs, on vérifie s'il existe dans la DB
+    	if(count($errors) < 1 ) {
+        
+			// Pour chaque tableau User où l'on définit $user, on vérifie si la clé username et password correspond a ce qui a été envoyé en $_POST.
+			$sql= "SELECT * FROM users WHERE username =? && password =?";
+			// echo $sql;
+			$sth = $dbh->prepare($sql);
+			$sth->execute(array($username, $password));
+			$user = $sth->fetchAll();
+			
+			$_SESSION['user'] = $user;
+			$_SESSION['logged_in'] = 'ok';
+			//print_r($_SESSION);
+			header('Location: semaine.php');
+			die("you should be gone");
+			exit;
+		} else {
+			$errors['resultat'] = 'Nom d\'utilisateur introuvable...';
+		}
+	die("formulaire posté mais rien");	
+	}
+} else{
+	header('Location: semaine.php');
+	exit;
+}
+
+?><!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css"/>
+</head>
+<body>
+	<div class="container">
+		<header class="phone">
+			<ul class="balibalo">
+				<li class="name_app"><h1>Yeti</h1></li>
+			</ul>
+		</header>
+		<div class="content">
+			<div class="phone">
+				<div class="logo">
+				<img src="img/logo.png" alt="Yeti"/>
+			</div>
+			<form class="connect" method="post">
+				<fieldset>
+					<ol>
+						<li class="decal">
+						<label class="disappear" for="username">Nom d'utilisateur</label>
+						<input id="username" class="area" name="username" type="text" placeholder="Nom d'utilisateur..." value="yoyo"/>
+						<?php echo message_erreur($errors, 'username');?>
+						</li>
+						<li class="decal">
+						<label class="disappear" for="password">Mot de passe</label>
+						<input id="password" class="area" name="password" type="password" placeholder="Mot de passe..." value="yoyo"/>
+						<?php echo message_erreur($errors, 'password');?>
+
+						</li>
+						<li class="moit" style="display:none">
+						<input id="connected" name="connected" type="checkbox"/>
+						<label for="connected">Rester connecté</label>
+						</li>
+						<li class="moit">
+						<a href="#">(Mot de passe oublié ?)</a>
+						</li>
+						<li class="connect_elem">
+						<label class="disappear" for="connexion">Se connecter</label>
+						<input id="connexion" name="connexion" type="submit" value="Se connecter"/>
+						</li>
+						<li id="bottomity" class="moit">
+						<a href="inscription.php">S'inscrire</a>
+						</li>
+					</ol>
+				</fieldset>
+			</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- semaine.php
+++ semaine.php
@@ -1,4 +1,29 @@
-<!doctype html>
+<?php
+require_once('config.inc.php');
+
+// vérifier s'il est loggué
+//echo '<pre>';
+//print_r($_SESSION);
+//exit;
+if($_SESSION['logged_in'] != 'ok') {
+	header('Location: index.php');
+	exit;
+}
+
+// la semaine de l'utilisateur dans la DB
+
+$sql= "SELECT * FROM tasks LEFT JOIN users ON users.id= tasks.user_id WHERE user_id = '".$_SESSION['user'][0]['id']."'";
+//echo $sql;
+
+$q =  $dbh ->query($sql);
+$tasks = $q->fetch(PDO::FETCH_ASSOC);
+foreach ($tasks as $id => $task) {
+	//echo $task; // Montrer les tâches
+}
+/*echo '<pre>';
+print_r($tasks);
+exit;*/
+?><!doctype html>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -6,126 +31,128 @@
     <link rel="stylesheet" href="css/styles.css" type="text/css"/>
 </head>
 <body>
-	<header class="phone">
-		<ul class="exception">
-			<li class="back"><a href="#">Mars</a></li>
-			<li><h1 class="space"><a class="user" href="#">Peter</a></h1></li>
-			<li class="more"><a href="#">+</a></li>
-		</ul>
-		<ul class="champs">
-			<li class="section_search">
-			<img class="search_logo" src="#" alt="search_logo"/>
-			<input id="search" name="search" type="text" placeholder="Rechercher"/>
-			</li>
-		</ul>
-	</header>
-	<div class="content">
-		<div class="phone">
+	<div id="semaine" class="container">
+		<header class="phone">
+			<ul class="exception">
+				<li class="back"><a href="mois.php">Mars</a></li>
+				<li><h1 class="space"><a class="user" href="#">Peter</a></h1></li>
+				<li class="more"><a href="#">+</a></li>
+			</ul>
+			<ul class="champs">
+				<li class="section_search">
+				<img class="search_logo" src="#" alt="search_logo"/>
+				<input id="search" name="search" type="text" placeholder="Rechercher"/>
+				</li>
+			</ul>
+		</header>
+		<div class="content">
+			<div class="phone">
+				<ul class="past_day">
+				<li>
+					<h3 class="date">Lundi 16 mars 2015</h3>
+				</li>
+				<li class="disappear">
+					<table class="planning">
+					</table>
+				</li>
+			</ul>
 			<ul class="past_day">
-			<li>
-				<h3 class="date">Lundi 16 mars 2015</h3>
-			</li>
-			<li class="disappear">
-				<table class="planning">
-				</table>
-			</li>
-		</ul>
-		<ul class="past_day">
-			<li>
-				<h3 class="date">Mardi 17 mars 2015</h3>
-			</li>
-			<li class="disappear">
-				<table class="planning">
-				</table>
-			</li>
-		</ul>
-		<ul class="active_day">
-			<li>
-				<h3 class="date">Mercredi 18 mars 2015</h3>
-			</li>
-			<li>
-				<table class="planning">
-					<tr>
-						<td class="tab_left">17:10</td>
-						<td class="double_line">Responsabilités & Grand pouvoir<br/><span class="prof">Oncle Ben</span></td>
-						<td class="tab_right">Parking B4</td>
-					</tr>
-					<tr>
-						<td class="tab_left_split">17:20</td>
-						<td class="tab_split"></td>
-						<td class="tab_right_split"></td>
-					</tr>
-					<tr>
-						<td class="tab_left">18:30</td>
-						<td class="double_line">Triste nouvelle<br/><span class="prof">Tante May</span></td>
-						<td class="tab_right">Domicile</td>
-					</tr>
-				</table>
-			</li>
-			<li>
+				<li>
+					<h3 class="date">Mardi 17 mars 2015</h3>
+				</li>
+				<li class="disappear">
+					<table class="planning">
+					</table>
+				</li>
+			</ul>
+			<ul class="active_day">
+				<li>
+					<h3 class="date">Mercredi 18 mars 2015</h3>
+				</li>
+				<li>
+					<table class="planning">
+						<tr>
+							<td class="tab_left">17:10</td>
+							<td class="double_line">Responsabilités & Grand pouvoir<br/><span class="prof">Oncle Ben</span></td>
+							<td class="tab_right">Parking B4</td>
+						</tr>
+						<tr>
+							<td class="tab_left_split">17:20</td>
+							<td class="tab_split"></td>
+							<td class="tab_right_split"></td>
+						</tr>
+						<tr>
+							<td class="tab_left">18:30</td>
+							<td class="double_line">Triste nouvelle<br/><span class="prof">Tante May</span></td>
+							<td class="tab_right">Domicile</td>
+						</tr>
+					</table>
+				</li>
+				<li>
+					<div class="todo">
+						<ul>
+							<li><input name="shaker" type="checkbox"/><label class="todo_right" name="shaker" for="shaker"><?php echo $task ?></label></li>
+							<li><input type="checkbox"/><input class="todo_right" type="text" value="Rejoindre Oncle Ben"/></li>
+							<li><input type="checkbox"/><input class="todo_right" type="text" value="Aller à la bibliothèque"/></li>
+							<li><input type="checkbox"/><input class="todo_right" type="text" value="Rentre à pied"/></li>
+							<li><input type="checkbox"/><input class="todo_right" type="text" value="Croiser Oncle Ben"/></li>
+							<li><input type="checkbox"/><input class="todo_right" type="text" value="Parler à Tante May"/></li>
+						</ul>
+						<form class="more">
+							<ul>
+								<li><input class="add" type="text" placeholder="Ajouter une tâche..."/><input class="ok" type="submit" value="OK"/></li>
+							</ul>
+						</form>
+					</div>
+				</li>
+			</ul>
+			<ul class="back_to_the_future">
+				<li>
+					<h3 class="date">Jeudi 19 mars 2015</h3>
+				</li>
+				<li class="disappear">
+					<table class="planning">
+					</table>
+				</li>
+				<li>
 				<div class="todo">
-					<ul>
-						<li><input type="checkbox"/><input class="todo_right" type="text" value="Acheter un chocolat"/></li>
-						<li><input type="checkbox"/><input class="todo_right" type="text" value="Rejoindre Oncle Ben"/></li>
-						<li><input type="checkbox"/><input class="todo_right" type="text" value="Aller à la bibliothèque"/></li>
-						<li><input type="checkbox"/><input class="todo_right" type="text" value="Rentre à pied"/></li>
-						<li><input type="checkbox"/><input class="todo_right" type="text" value="Croiser Oncle Ben"/></li>
-						<li><input type="checkbox"/><input class="todo_right" type="text" value="Parler à Tante May"/></li>
-					</ul>
-					<form class="more">
-						<ul>
-							<li><input class="add" type="text" placeholder="Ajouter une tâche..."/><input class="ok" type="submit" value="OK"/></li>
+						<ul class="future_adjust">
+							<li><input type="checkbox"/><input class="todo_right" type="text" value="Préparer les funérailles"/></li>
 						</ul>
-					</form>
-				</div>
-			</li>
-		</ul>
-		<ul class="back_to_the_future">
-			<li>
-				<h3 class="date">Jeudi 19 mars 2015</h3>
-			</li>
-			<li class="disappear">
-				<table class="planning">
-				</table>
-			</li>
-			<li>
-			<div class="todo">
-					<ul class="future_adjust">
-						<li><input type="checkbox"/><input class="todo_right" type="text" value="Préparer les funérailles"/></li>
-					</ul>
-					<form class="more">
+						<form class="more">
+							<ul>
+								<li><input class="add" type="text" placeholder="Ajouter une tâche..."/><input class="ok" type="submit" value="OK"/></li>
+							</ul>
+						</form>
+					</div>
+				</li>
+			</ul>
+			<ul class="back_to_the_future">
+				<li>
+					<h3 class="date">Vendredi 20 mars 2015</h3>
+				</li>
+				<li>
+					<table class="planning">
+						<tr>
+							<td>10:00</td>
+							<td>Enterrement</td>
+							<td class="tab_right">Cimetière</td>
+						</tr>
+					</table>
+				</li>
+				<li>
+					<div class="todo">
 						<ul>
-							<li><input class="add" type="text" placeholder="Ajouter une tâche..."/><input class="ok" type="submit" value="OK"/></li>
 						</ul>
-					</form>
-				</div>
-			</li>
-		</ul>
-		<ul class="back_to_the_future">
-			<li>
-				<h3 class="date">Vendredi 20 mars 2015</h3>
-			</li>
-			<li>
-				<table class="planning">
-					<tr>
-						<td>10:00</td>
-						<td>Enterrement</td>
-						<td class="tab_right">Cimetière</td>
-					</tr>
-				</table>
-			</li>
-			<li>
-				<div class="todo">
-					<ul>
-					</ul>
-					<form class="more">
-						<ul>
-							<li><input class="add" type="text" placeholder="Ajouter une tâche..."/><input class="ok" type="submit" value="OK"/></li>
-						</ul>
-					</form>
-				</div>
-			</li>
-		</ul>
+						<form class="more">
+							<ul>
+								<li><input class="add" type="text" placeholder="Ajouter une tâche..."/><input class="ok" type="submit" value="OK"/></li>
+							</ul>
+						</form>
+					</div>
+				</li>
+			</ul>
+			</div>
 		</div>
 	</div>
 </body>
