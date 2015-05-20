b41f78b8fcaf7db971e41edb19c86a2c70b3c42e
Tibroux <tiby.voesters@gmail.com>
2015-05-20 13:22:15 +0200

    ninja

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=tibyvoesters', 'tibyvoesters', '0DU08s0fsaEGcxEO');
+    $dbh = new PDO('mysql:host=localhost;dbname=nameofdb', 'nameofdb', 'passwordofdb');
 	$dbh->exec('SET CHARACTER SET utf8');
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
