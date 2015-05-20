8c021dd2e8b01db6a56404176d177094946150bc
Tibroux <tiby.voesters@gmail.com>
2015-05-20 13:42:46 +0200

    secure

--- db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=nameofdb', 'nameofdb', 'passwordofdb');
-	$dbh->exec('SET CHARACTER SET utf8');
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die('connection failed');
-}
